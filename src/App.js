4f43ac908974423c7279e53422d802b05bbd26f1
SarahOh92 <201124081oek@gmail.com>
2022-04-15 11:01:10 +0900

    05 리액트 state,class,생명주기 학습

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-// function Fruit(props){
-//   console.log(props);
-//   return (
-//     <h1>{props.fav} 맛있어요</h1>
-//   )
-// }
-function Fruit({name, picture, rating}){
-  return( 
-    <div>
-      <h2>{name} 맛있어요</h2>
-      <h4>{rating} / 5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-    )
-}
-const fruitILike = [
-  {
-    id:1,
-    name: 'banana',
-    image:'http://qwerew.cafe24.com/images/banana.png',
-    rating: 5
-  },
-  {
-    id:2,
-    name: 'apple',
-    image:'http://qwerew.cafe24.com/images/apple.png',
-    rating: 4.9
-  },
-  {
-    id:3,
-    name: 'melon',
-    image:'http://qwerew.cafe24.com/images/melon.jpg',
-    rating: 4
-  }
-];
 
-function App() {
-  return (
-    <div>
-      {fruitILike.map((dish) => (<Fruit key={dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating} />)
-    )}
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log('constructor 실행');
+  }
+
+  state = {count:0};
+  add = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  minus = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  componentDidMount(){
+    console.log('componentDidMount 실완')
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate 실완')
+  }
+  componentWillUnmount(){
+    console.log("비번뭐야");
+  }
+  render(){
+    console.log('render 실완')
+    return (
+      <div>
+        <h1>번호는{this.state.count}</h1>
+        <button onClick = {this.add}>더하기</button>
+        <button onClick = {this.minus}>빼기</button>
+      </div>)
+  }
 }
-Fruit.propTypes ={
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+
 
 export default App;
