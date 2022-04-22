c98b8c991b319477530d864729aac0f5e313f74b
SarahOh92 <201124081oek@gmail.com>
2022-04-22 16:51:49 +0900

    Updates

---
+++ precache-manifest.03d8ff33b03a3431c92a0e835be75324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0e3bd6f35c567d89dbdccd93af3b71",
+    "url": "/movie_app_2022/index.html"
+  },
+  {
+    "revision": "e064d1fab34ed1272068",
+    "url": "/movie_app_2022/static/css/main.bb533936.chunk.css"
+  },
+  {
+    "revision": "d761f6f4f496152e3a59",
+    "url": "/movie_app_2022/static/js/2.6bb022e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2022/static/js/2.6bb022e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e064d1fab34ed1272068",
+    "url": "/movie_app_2022/static/js/main.0a8b3486.chunk.js"
+  },
+  {
+    "revision": "b4bd60b86ff74e150aaf",
+    "url": "/movie_app_2022/static/js/runtime-main.e76b7f5b.js"
+  }
+]);
